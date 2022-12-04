86bb451e13d050b95476414a2d3f98511886acd4
krindioom <yoerfrost1234@gmail.com>
2022-12-04 20:36:04 +0700

    my_paw_added

---
+++ main1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+double my_pow(double indicator, unsigned int degree)    
+{
+	double finalNumber = 1;
+
+	if (degree == 0)
+		return 1;
+
+	for (int i = 0; i < degree; i++)
+	{
+		finalNumber *= indicator;
+	}
+
+	return finalNumber;
+}
+
+int main()
+{
+	int degree;
+	double indicator;
+
+	cin >> indicator >> degree;
+	double number = my_pow(indicator, degree);
+
+	cout << number;
+
+	return 0;
+}
